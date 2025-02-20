a3350b7484f8fab52fa13d401aa6b0463a9f12a8
shaanMaheshwari <ShaanMaheshwari2006@gmail.com>
2025-02-20 18:37:43 +0000

    working 2/20

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -4,10 +4,13 @@ public class BST implements BSTInterface
 {
     private TreeNode root;
 
+    //main constructor
     public BST(){
         root = null;
     }
 
+
+    //add method
     public void add(Comparable newVal){
         if(root == null){
             root = new TreeNode(newVal, null, null);
@@ -18,14 +21,78 @@ public class BST implements BSTInterface
         }
     }
 
+
+    //add helper function
     private void addHelper(Comparable val, TreeNode child, TreeNode parent){
         if(child == null){
             if(val.compareTo(parent.getValue()) <= 0){
-                parent.setLeft(val);
+                parent.setLeft(new TreeNode(val));
             } else { 
-                parent.setRight(val);
+                parent.setRight(new TreeNode(val));
+            }
+        } else {
+            if(val.compareTo(child.getLeft()) <= 0){
+                addHelper(val, parent.getLeft(), parent);
+            } else {
+                addHelper(val, parent.getRight(), parent);
             }
         }
     }
+
+
+
+    //in order traversal
+    public void printInOrder(){
+        if(root != null){
+            inOrderHelper(root.getLeft());
+            System.out.print(root.getValue());
+            inOrderHelper(root.getRight());
+        }
+    }
+
+    private void inOrderHelper(TreeNode child){
+        if(child != null){
+            inOrderHelper(child.getLeft());
+            System.out.print(" " + child.getValue());
+            inOrderHelper(child.getRight());
+        }
+    }
+
+
+    //pre order traversal
+    public void printPreOrder(){
+        if(root != null){
+            System.out.print(root.getValue());
+            preOrderHelper(root.getLeft());
+            preOrderHelper(root.getRight());
+        }
+    }
+
+    private void preOrderHelper(TreeNode child){
+        if(child != null){
+            System.out.print(" " + child.getValue());
+            inOrderHelper(child.getLeft());
+            inOrderHelper(child.getRight());
+        }
+    }
+
+
+    //post order traversal
+    public void printPostOrder(){
+        if(root != null){
+            postOrderHelper(root.getLeft());
+            postOrderHelper(root.getRight());
+            System.out.print(root.getValue());
+        }
+    }
+
+    private void postOrderHelper(TreeNode child){
+        if(child != null){
+            inOrderHelper(child.getLeft());
+            inOrderHelper(child.getRight());
+            System.out.print(" " + child.getValue());
+        }
+    }
+
 
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -5,10 +5,34 @@
  * TODO: Program Description
  */
 
- import java.lang.Comparable;
+import java.lang.Comparable;
 
- public class BSTTester {
-   public static void main(String[] args) {
-   
-	
- }
+public class BSTTester {
+  public static void main(String[] args) {
+
+    BST searchTree = new BST();
+
+    searchTree.add("O");
+    searchTree.add("R");
+    searchTree.add("C");
+    searchTree.add("H");
+    searchTree.add("A");
+    searchTree.add("R");
+    searchTree.add("D");
+    searchTree.add("Q");
+    searchTree.add("U");
+    searchTree.add("I");
+    searchTree.add("N");
+    searchTree.add("C");
+    searchTree.add("E");
+
+    System.out.print("In order: ");
+    searchTree.printInOrder();
+
+    System.out.print("Pre order: ");
+    searchTree.printPreOrder();
+
+    System.out.print("Post order: ");
+    searchTree.printPostOrder();
+  }
+}
